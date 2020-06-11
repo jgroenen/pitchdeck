dd61a3d11bc21c34353fca95fbfa96109e9a7b9e
Johan Groenen <johan.groenen+github@gmail.com>
2020-06-11 20:12:48 +0200

    removed some legacy

--- index.php
+++ index.php
@@ -50,24 +50,8 @@
     <meta name="author" content="Johan Groenen (c) 2015">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/assets/css/reset.css">
-    
-<?php if ($_SERVER["HTTP_HOST"] !== "pitchdeck.tiltshift.nl") { ?>
     <link rel="stylesheet" href="/assets/css/style.css">
-<?php } else { ?>
-    <link rel="stylesheet" href="/assets/css/tiltshift.css">
-<?php } ?>
-
     <script src="/assets/js/vendor/require/require-2.1.15/require.js" data-main="/assets/js/app/bootstrap.js"></script>
-    
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-37914920-2', 'auto');
-        ga('send', 'pageview');
-    </script>
-    
 </head>
 <body>
 </body>
