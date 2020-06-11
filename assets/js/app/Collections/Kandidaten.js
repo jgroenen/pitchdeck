a1019062837fb113e9d26c651a71df22dde925d1
Johan Groenen <johan.groenen+github@gmail.com>
2020-06-11 20:25:07 +0200

    Delete Kandidaten.js

--- assets/js/app/Collections/Kandidaten.js
+++
@@ -1,9 +0,0 @@
-define([
-    'settings'
-], function (settings) {
-    return Backbone.Collection.extend({
-        parse: function (data) {
-            return data.members;
-        }
-    });
-});
